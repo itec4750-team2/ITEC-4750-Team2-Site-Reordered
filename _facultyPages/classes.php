2ab867982190e5eb402f0ba1febdbcba54b10b95
Kateknm <31115258+Kateknm@users.noreply.github.com>
2017-09-03 10:36:18 -0700

    Separation of Concerns - Faculty Classes

--- _facultyPages/classes.php
+++ _facultyPages/classes.php
@@ -3,39 +3,53 @@ include('../_templates/facultyHeader.php');
 include('../_templates/facultyNav.php');
 ?>
 	
-	<!-- Main Content Section-->
-	<div class="wrapper">
+<!-- Main Content Section-->
+<div class="wrapper">
 
-	<!-- Builds table for classes. Placeholders are present currently other than Spring 2017 Capstone. -->
-		<div id="main">
-			<h2 class="center">Your Classes</h2>
+<!-- Builds table for classes. If classes have Expired the are not pulled. KM 9/2/17 -->
+<div id="main">
+	<h2 class="center">Your Classes</h2>
+	  
+	<?php
+
+	if(isset($_SESSION['LoginID'])){
+	
+	require("../_php/_objects/class_do.php");	
+	//$root = realpath($_SERVER["DOCUMENT_ROOT"]);
+	//require("$root/_php/_objects/class_do.php");  
+
+	$classdo = new Class_DO();
+	}
+	?>
+
+	<table>
+		<tr>
+			<th>Class ID</th>
+			<th>Class Number</th>
+			<th>Class Name</th>
+			<th>Semester</th>
+			<th>Class Expire Date</th>
+		</tr>
+
+	<?php
+	//calls class data object and loads table data by LoginID
+	$rows=$classdo->loadByLoginID($_SESSION['LoginID']);
+	
+	//builds table with class data
+	foreach ($rows as $value){
+		echo "<tr>";
+		echo '<td><a href="class_page.php?id='.$value['ClassID'].'">'.$value['ClassID'].'</a></td>';
+		echo "<td>".$value['ClassNO']."</td><td>".$value['ClassName']."</td>";
+		echo "<td>".$value['SemesterName']. " ".$value['Year']."</td>";
+		echo "<td>". $value['ExpDate']."</td></tr>";
+	}
+	?>			   
+
+	</table>
 		
-		<!--
-		--- -- --- WORK FLAG
-		--- Thie classes need to be hidden for expired classes. 
-		--- Calculation should be done on the faculty_classes page. KM -- 8/31 AM
-		-->
-			<table>
-			  <tr>
-				<th>Class ID</th>
-				<th>Class Number</th>
-				<th>Class Name</th>
-				<th>Semester</th><th></th>
-				<th>Class Expire Date</th>
-			  </tr>
-			  
-			<?php echo $FacClasses; ?> <!-- Sarah this is set up to echo table rows and table data.-->
-
-			</table>
-
-			<hr>
-			
-		</div>
 	</div>
-	
-	<footer>
-			
-	</footer>
-	
+</div>
+
+<?php include("../_templates/footer.php");?>
 </body>
 </html>
