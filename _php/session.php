da93625ed4e51e2de23a4917431397be90bfc734
Kateknm <31115258+Kateknm@users.noreply.github.com>
2017-09-03 10:59:39 -0700

    Session Statements Removed
    Not needed after switch to model dataobject.

--- _php/session.php
+++ _php/session.php
@@ -15,9 +15,7 @@
 	$Locked = "";
 	$ErrorBlock = "";
 	$Token = ""; 
-	$FacClasses = "";
-	$StuClasses = "";
-	
+
    //Read your session (if it is set)
    	if(isset($_SESSION['LoginID'])) $LoginID = $_SESSION['LoginID'];
 	if(isset($_SESSION['FName'])) $FName = $_SESSION['FName'];
@@ -29,8 +27,5 @@
 	if(isset($_SESSION['Locked'])) $Locked = $_SESSION["Locked"];
 	if(isset($_SESSION['ErrorBlock'])) $ErrorBlock = $_SESSION["ErrorBlock"];
 	
-	//working on output for classes
-	if(isset($_SESSION['FacClasses'])) $FacClasses = $_SESSION["FacClasses"];
-	if(isset($_SESSION['StuClasses'])) $StuClasses = $_SESSION["StuClasses"];
   
 ?>
