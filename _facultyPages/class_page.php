13eac96634eb49ae91d56a913e7bd25c52d8f8a1
Kateknm <31115258+Kateknm@users.noreply.github.com>
2017-09-08 21:44:11 -0700

    class_page.php
    Adjusted indentation
    Added Links to Student Info
    Added Faculty Names to Table

--- _facultyPages/class_page.php
+++ _facultyPages/class_page.php
@@ -1,60 +1,112 @@
 <?php 
+// ++++ Change: Adjusted indentation 9/7 KM ++++
 include('../_templates/facultyHeader.php');
 include('../_templates/facultyNav.php');
-if(isset($_GET['id'])){$ClassID = $_GET['id'];}
-	if(!empty($_SESSION['LoginID'] && $ClassID )){
-	
-	require("../_php/_objects/class_do.php");	
-	require("../_php/_models/class_model.php");
-	require("../_php/_objects/stu_do.php");
-
-	$classdo = new Class_DO();
-	//calls class data object and loads table data by LoginID
-	$classpage=$classdo->classPage($ClassID);
-		foreach ($classpage as $value){
-		$ClassNO = $value['ClassNO'];
-		$ClassName=$value['ClassName'];
-		$SemesterName=$value['SemesterName'];
-		$Year = $value['Year'];
-		$ExpDate = $value['ExpDate'];
-		$SemesterID = $value['SemesterID'];
-		}
+require('../_php/_objects/class_do.php');	
+require('../_php/_models/class_model.php');
+// ++++ Change: Moved to this procedure to class_assign_do (previously part of stu_do) 9/5 KM ++++
+require('../_php/_objects/class_assign_do.php');
 ?>
-	
 <!-- Main Content Section-->
 <div class="wrapper">
-<div id="main">
-	<h1> <?php echo $ClassName . " - ". $SemesterName?></h1>
-<div>
-
-<table>
-<tr><th>Class Item Number</th><td><?php echo $ClassID;?></td></tr>
-<tr><th>Class Number</th><td><?php echo $ClassNO;?></td></tr>
-<tr><th>Class Name</th><td><?php echo $ClassName;?></td></tr>
-<tr><th>Class Expires</th><td><?php echo $ExpDate;?></td></tr>
-<tr><th>Semester</th><td><?php echo $SemesterName." ".$Year;?></td></tr>
-</table>
-<div><br/><br/>
-<table>
-<tr><class="one"><?php echo '<a href="delete_class.php?id='.$value['ClassID'].'">'."Delete Class </a>
-<class='one'><br/><br/>";?><?php echo '<a href="update_class.php?id='.$value['ClassID'].'">'."Update Class</a>";?></tr>
-</table><br/><br/></div>
-<table>
-<tr><th>Student ID</th><th>Student Name</th><th>Email</th></tr>
-
-<?php 
-$studo = new Stud_DO();	
-$rows=$studo->listClassStuds($_GET['id']);
-foreach ($rows as $value){
-echo '<tr><td><a href="stud_mgmt_pg.php?id='. $value['LoginID'] . '">'. $value['LoginID'].'</a></td>';
-echo "<td>" . $value['FName'] . " " . $value['LName'] . "</td>";
-echo "<td>" . $value['Email'] . "</td></tr>"; 
-}
-?>
-</table>
-</div>
-</div>
-<?php }
-include("../_templates/footer.php");?>
+	<main>
+		<?php 
+			if(isset($_GET['cid'])){
+				$ClassID = $_GET['cid'];
+				}
+			if(!empty($_SESSION['LoginID'] && $ClassID )){
+				// --------------- Class Information -------------
+				// ++++ Change: Passed currently selected ClassID & $_SESSION['LoginID'] ++++
+				$classdo = new Class_DO();
+				$classpage=$classdo->classPage($ClassID, $_SESSION['LoginID']);			
+				foreach ($classpage as $value){
+					$ClassNO = $value['ClassNO'];
+					$ClassName=$value['ClassName'];
+					$SemesterName=$value['SemesterName'];
+					$Year = $value['Year'];
+					$ExpDate = $value['ExpDate'];
+					$SemesterID = $value['SemesterID'];
+				}
+		?>	
+			<!-- ++++ change: added the word Semester to heading ++++ 9/5 KM-->
+			<h1> <?php echo $ClassName . ' - '. $SemesterName . ' Semester' ?></h1>
+				<div>
+					<table>
+						<!-- Gives Class Information Recap -->
+						<tr><th>Class Item Number</th><td><?php echo $ClassID;?></td></tr>
+						<tr><th>Class Number</th><td><?php echo $ClassNO;?></td></tr>
+						<tr><th>Class Name</th><td><?php echo $ClassName;?></td></tr>
+						<tr><th>Class Expires</th><td><?php echo $ExpDate;?></td></tr>
+						<tr><th>Semester</th><td><?php echo $SemesterName.' '.$Year;?></td></tr>
+						<!-- ++++ change: Added faculty name to table +++ 9/5 KM-->
+						<tr>
+							<th>Faculty Name</th>
+							<?php
+							// ----------------- Get Current Insts --------------
+								$instr = new CA_DO();
+								$facs=$instr->listClassInstrs($value['ClassID']);
+								if($facs){
+									$i=0;
+									foreach ($facs as $val){
+										$FID = $val['LoginID']; 
+										$FName = $val['FName'];
+										$LName = $val['LName'];
+										if($i==0){ // First instructor
+											echo '<td>'. $FName .' '. $LName.'</td>';
+										}
+										else{ //In case of multiple instructors.
+											echo '<tr>';
+											echo '<th></th>';
+											echo '<td>'. $FName .' '. $LName.'</td>';
+											echo '</tr>';
+											}
+										$i++;
+									}
+								}
+								else{
+									echo '<td> No Instructor Listed. </td>';
+								}
+							?>
+					</tr>
+					</table>
+					<br/>
+					<br/>
+					<table>
+						<tr>
+							<?php 
+								//Update and Delete class links.
+								echo '<a href="delete_class.php?cid='.$value['ClassID'].'">'.'Delete Class </a>'; // delete class
+								echo '<br/><br/>';
+								echo '<a href="update_class.php?cid='.$value['ClassID'].'">'.'Update Class</a>'; // update class
+							?>
+						</tr>
+					</table>
+				</div>
+				<br/>
+				<br/>
+				<table>
+					<!-- ++++ Change: Took out student's id and added group 9/5 KM ++++ -->
+					<th>Student Name</th><th>Email</th><th>Group</th></tr>
+					<?php 
+						// Lists student's assigned to a class.
+						// ++++ Change: Moved to class_assign_do from stu_do 9/5 KM ++++
+						$cado = new CA_DO();
+						$rows=$cado->listClassStuds($ClassID);
+						foreach ($rows as $value){
+							echo '<tr>';
+								// ++++ Change: Took out id & linked student_mgt_pg to Name 9/5 KM ++++ 
+								echo '<td>' . '<a href="stud_mgmt_pg.php?stid=' . $value['LoginID'] . '">' . $value['FName'] . ' ' . $value['LName'] . '</a></td>'; // links to student_mgt_pg for this student
+								// ++++ Change: Added mail to email link 9/5 KM ++++ 
+								echo '<td>' . '<a href="mailto:' . $value['Email'].'">' . $value['Email'] . '</a></td>';
+								// ++++ Change: Added group linked to class_group stub (to be developed soon) 9/5 KM ++++ 
+								echo '<td>' . '<a href="class_group.php?gid='.$value['GroupID'].'&gname='.$value['GroupName'].'">' . $value['GroupName'] . '</a></td>'; // links to group page for this group
+							echo '</tr>';
+						}	
+					?>
+				</table>
+		<?php }?>	
+	</main>
+</div><!--End Wrapper -->
+<?php include('../_templates/facfooter.php');?>
 </body>
 </html>
