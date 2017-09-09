8b73a02231115f29e33743f03d1b747cd9942f22
Kateknm <31115258+Kateknm@users.noreply.github.com>
2017-09-08 23:54:54 -0700

    class_model
    Added Indentation

--- _php/_models/class_model.php
+++ _php/_models/class_model.php
@@ -1,69 +1,62 @@
 <?php
+//	++++ Added Indentation 9/8 KM ++++
+	include("../_php/config.php");
+	class Classes{
+		private $LoginID;  
+		private $ClassID;
+		private $ClassNO;
+		private $ClassName;
+		private $SemesterID;
+		private $ExpDate; 
+		private $do;
+		
+		public function __get($property){
+			if(property_exists($this, $property)){
+				return $this->$property;
+			}
+		}
 
-include("../_php/config.php");
-// $root = realpath($_SERVER["DOCUMENT_ROOT"]);
-// include("$root/_php/config.php");
+		public function __construct($arr){
+			if(isset($arr["LoginID"])){
+				$this->LoginID = $arr["LoginID"];	  
+				$this->ClassID = $arr["ClassID"];
+				$this->ClassNO = $arr["ClassNO"];
+				$this->ClassName = $arr["ClassName"];
+				$this->SemesterID = $arr["SemesterID"];
+				$this->ExpDate= $arr['ExpDate'];
+				$this->do = new Class_DO();
+			}
+		}
 
-class Classes{
-private $LoginID;  
-private $ClassID;
-private $ClassNO;
-private $ClassName;
-private $SemesterID;
-private $ExpDate; 
-//private $SemesterName;
-//private $Year;
-private $do;
+		private function all_params(){
+			$params = array(
+				"LoginID" => $this->LoginID,
+				"ClassID" => $this->ClassID,
+				"ClassNO" => $this->ClassNO,
+				"ClassName" => $this->ClassName,
+				"SemesterID" => $this->SemesterID,
+				"ExpDate" => $this->ExpDate);
+			return $params;
+		}
+		
+		private function idOnly(){
+			$params = array(
+				"LoginID" => $this->LoginID,
+				"ClassID" => $this->ClassID);
+			return $params;
+		}
 
-public function __get($property){
-	if(property_exists($this, $property)){
-	return $this->$property;
-}}
+		public function createClass(){
+			return $this->do->createClass($this->all_params());
+		}
 
-public function __construct($arr){
-if(isset($arr["LoginID"])){
-	$this->LoginID = $arr["LoginID"];	  
-	$this->ClassID = $arr["ClassID"];
-	$this->ClassNO = $arr["ClassNO"];
-	$this->ClassName = $arr["ClassName"];
-	$this->SemesterID = $arr["SemesterID"];
-	$this->ExpDate= $arr['ExpDate'];
-//	$this->SemesterName = $arr['SemesterName'];
-//	$this->Year = $arr['Year'];
-	$this->do = new Class_DO();
-}}
+		public function updateClass(){
+			$r = $this->do->updateClass($this->ClassID, $this->all_params());
+			return $r;
+		}
 
-private function all_params(){
-	$params = array(
-	"LoginID" => $this->LoginID,
-	"ClassID" => $this->ClassID,
-	"ClassNO" => $this->ClassNO,
-	"ClassName" => $this->ClassName,
-	"SemesterID" => $this->SemesterID,
-	"ExpDate" => $this->ExpDate
-);
-return $params;
-}
-private function idOnly(){
-	$params = array(
-	"LoginID" => $this->LoginID,
-	"ClassID" => $this->ClassID
-);
-return $params;
-}
-
-public function createClass(){
-	return $this->do->createClass($this->all_params());
-}
-
-public function updateClass(){
-	$r = $this->do->updateClass($this->ClassID, $this->all_params());
-	return $r;
-}
-
-public function deleteClass(){
-	return $this->do->deleteClass($this->idOnly());
-}
-
-}
+		public function deleteClass(){
+			return $this->do->deleteClass($this->idOnly());
+		}
+	}
  ?>
