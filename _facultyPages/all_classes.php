8f17c33901dd417a999afeadab33e4b106c9e5c6
Kateknm <31115258+Kateknm@users.noreply.github.com>
2017-09-08 20:30:43 -0700

    all_classes.php
    Page lists all current classes from table.
    Added Log in Error Msg
    Adjusted indentation.

--- _facultyPages/all_classes.php
+++ _facultyPages/all_classes.php
@@ -1,57 +1,43 @@
 <?php 
+// ++++ Change: Adjusted indentation 9/8 KM ++++
 include('../_templates/facultyHeader.php');
 include('../_templates/facultyNav.php');
+require('../_php/_objects/class_do.php');
 ?>
-	
 <!-- Main Content Section-->
 <div class="wrapper">
-
-<!-- Builds table for classes. If classes have Expired the are not pulled. KM 9/2/17 -->
-<div id="main">
-	<h2 class="center">ITEC Classes</h2>
-	  
-	<?php
-
-	if(isset($_SESSION['LoginID'])){
-	
-	require("../_php/_objects/class_do.php");	
-	//$root = realpath($_SERVER["DOCUMENT_ROOT"]);
-	//require("$root/_php/_objects/class_do.php");  
-
-	$classdo = new Class_DO($_SESSION['LoginID']);
-	}
-	?>
-
-	<table>
-		<tr>
-			<th>Class ID</th>
-			<th>Class Number</th>
-			<th>Class Name</th>
-			<th>Semester</th>
-			<th>Class Expire Date</th>
-		</tr>
-
-	<?php
-	//calls class data object and loads table data 
-	//checks that Role='faculty'--KM 9/3/17
-	$rows=$classdo->loadAll($_SESSION['LoginID']);
-	
-	//builds table with class data
-	//links ID to information page where faculty can update or delete class --KM 9/3/17
-	
-	foreach ($rows as $value){
-		echo '<td><a href="class_page.php?id='.$value['ClassID'].'">'.$value['ClassID'].'</a></td>';
-		echo "<td>".$value['ClassNO']."</td><td>".$value['ClassName']."</td>";
-		echo "<td>".$value['SemesterName']. " ".$value['Year']."</td>";
-		echo "<td>". $value['ExpDate']."</td></tr>";
-	}
-	?>			   
-
-	</table>
-		
-	</div>
-</div>
-
-<?php include("../_templates/footer.php");?>
+	<!-- Builds all classes table. If classes have Expired the are not pulled. KM 9/2/17 -->
+	<main>
+		<h2 class="center">ITEC Classes</h2>
+		<?php
+		if(isset($_SESSION['LoginID'])){		
+			$classdo = new Class_DO($_SESSION['LoginID']);		
+			$rows=$classdo->loadAll($_SESSION['LoginID']);
+		?>
+			<table>
+				<tr>
+					<th>Class ID</th>
+					<th>Class Number</th>
+					<th>Class Name</th>
+					<th>Semester</th>
+					<th>Class Expire Date</th>
+				</tr>
+		<?php
+				foreach ($rows as $value){
+					echo '<td><a href="class_page.php?cid='.$value['ClassID'].'">'.$value['ClassID'].'</a></td>'; // link to class_page
+					echo '<td>'.$value['ClassNO'].'</td><td>'.$value['ClassName'].'</td>';
+					echo '<td>'.$value['SemesterName']. ' '.$value['Year'].'</td>';
+					echo '<td>'. $value['ExpDate'].'</td></tr>';
+				}
+	  echo '</table>';		
+		} // End if faculty logged in. 
+		// ++++ Change: Added error msg for no $_SESSION['LoginID'] 9/8 KM ++++
+		else{
+			echo '<div class = "error"> Only faculty can view this page. <br/> Please log in... </div>';
+		}
+		?>			   
+	</main>
+</div> <!-- End Wrapper -->
+<?php include('../_templates/facfooter.php');?>
 </body>
 </html>
