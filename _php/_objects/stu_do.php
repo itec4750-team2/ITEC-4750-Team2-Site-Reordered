c551551cb282d1623908b5be74874f6695680995
Kateknm <31115258+Kateknm@users.noreply.github.com>
2017-09-09 00:03:46 -0700

    stu_do
    Added Indentation
    Moved listClassStuds to class_assign_do
    Added listStud to list student info on the student_mgmt_pg

--- _php/_objects/stu_do.php
+++ _php/_objects/stu_do.php
@@ -1,35 +1,27 @@
 <?php
-
-class Stud_DO{
-
-// -- Create
-	
-// -- Read All
-
-// -- Load By Class		
-
-public function listClassStuds($ClassID){
-		if(!empty($ClassID)){
-		include("../_php/config.php");
-
-		$sql = "SELECT login.LoginID, class.ClassID, login.LName, login.FName, login.Email
-			FROM(((class
-			INNER JOIN semester ON class.SemesterID=semester.SemesterID)
-			INNER JOIN class_assign ON class.ClassID=class_assign.ClassID)
-			INNER JOIN login ON class_assign.LoginID = login.LoginID)
-			WHERE DATEDIFF(ExpDate, NOW())>0 && login.Role = 'Student' && class.ClassID='$ClassID'";
+	class Stud_DO{
+	// -- Create
 		
-		$getStuds = mysqli_query($con, $sql);
-		
-		//output data of each row
-		$all_rows = array();
-		while($row = mysqli_fetch_array($getStuds)){$all_rows[]=$row;}
-		return $all_rows;}}
+	// -- Read 	
 
-// -- Student Info Page -- Update and Delete Accessible
-
-// -- Update Student Info				
-		
-// -- Delete Student
-}
+		// -- Student Info Page -- Update and Delete Accessible
+		public function listStud($StID){
+			if(!empty($StID)){
+				include("../_php/config.php");
+				$sql = "SELECT login.LoginID, login.LName, login.FName, login.Email
+					FROM login
+					WHERE login.LoginID = '$StID' && Role='Student'";	
+				$getStud = mysqli_query($con, $sql);		
+				//output data of each row
+				$all_rows = array();
+				while($row = mysqli_fetch_array($getStud)){
+					$all_rows[]=$row;
+				}
+				return $all_rows;
+			}
+		}
+	// -- Update Student Info				
+			
+	// -- Delete Student
+	}
 ?>
