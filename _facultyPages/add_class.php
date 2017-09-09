88a4e9261fb2c87b56092ca563248d24f46d607a
Kateknm <31115258+Kateknm@users.noreply.github.com>
2017-09-08 21:14:40 -0700

    add_class
    Adjusted indentation
    Added comments

--- _facultyPages/add_class.php
+++ _facultyPages/add_class.php
@@ -1,92 +1,79 @@
 <?php 
+// ++++ Change: Adjusted indentation 9/8 KM ++++
+/* --
+--- -- --- WORK FLAG
+---This page still needs work. Maybe use a <datalist> populated with classes offered. -- 9/8 KM
+--- -- */
 include('../_templates/facultyHeader.php');
 include('../_templates/facultyNav.php');
-
-include("../_php/config.php");
-require("../_php/_models/class_model.php");	
-require("../_php/_objects/class_do.php");
-require("../_php/_objects/drop_do.php");
-
-//$root = realpath($_SERVER["DOCUMENT_ROOT"]);
-//require("$root/_php/_models/class_model.php"); 
-//require("$root/_php/_objects/class_do.php"); 
-//require("$root/_php/_objects/drop_do.php");
-//include("$root/_php/config.php");
-
-$dropdo = new Drop_DO($_SESSION['LoginID']);
+include('../_php/config.php');
+require('../_php/_models/class_model.php');	
+require('../_php/_objects/class_do.php');
+require('../_php/_objects/drop_do.php');
 ?>
 	
 <!-- Main Content Section-->
 <div class="wrapper">
-<div id="main">
+	<div id="main">
+		<form name="create-class" method="POST">
+			<div>
+				<label for="ClassID">Class Item Number</label>
+				<input type="number" name="ClassID" id="ClassID" placeholder="Item Number" required>
+			</div>
 
-<form name="create-class" method="POST">
+			<div>
+				<label for="ClassNO">Class Number</label>
+				<input type="text" name="ClassNO" id="ClassNO" placeholder="ITEC Number" required>
+			</div>
 
-<div>
-	<label for="ClassID">Class Item Number</label>
-	<input type="number" name="ClassID" id="ClassID" placeholder="Item Number" required>
+			<div>
+				<label for="ClassName">Class Name</label>
+				<input type="text" name="ClassName" id="ClassName" placeholder="Class Name" required>
+			</div>
+
+			<div>
+				<label for="ExpDate">Class Expires</label>
+				<input type="date" name="ExpDate" id="ExpDate" placeholder="Expiration Date" required>
+			</div>
+			<!----------------- Semester DropBox ------------>
+			<div>
+				<label for="SemesterID">Semester</label>
+				<?php
+					// -- Calls semSelect dropdown box from drop_do.php
+					$dropdo = new Drop_DO($_SESSION['LoginID']);
+					$rows=$dropdo->semSelect();
+					echo '<select name="SemesterID" required>'; // Open
+						foreach ($rows as $ddo) {
+						  echo '<option value="'.$ddo['SemesterID'].'">'.$ddo['SemesterName'].' '.$ddo['Year'].'</option>';
+						}
+					 echo '</select>';// Close
+
+				?>
+			</div>
+
+			<div class="one"> <input type="submit" value="Add Class" name="AddClass" id="AddClass"></div>
+			<!----------------- Add Class ------------------>
+			<div>
+				<?php 
+					if(isset($_POST['AddClass'])){
+						$newClass = new Classes(array(
+							'LoginID' => $_SESSION['LoginID'],
+							'ClassID' => $_POST['ClassID'],
+							'ClassNO' => $_POST['ClassNO'],
+							'ClassName' => $_POST['ClassName'],
+							'SemesterID' => $_POST['SemesterID'],
+							'ExpDate' => $_POST['ExpDate']));
+						$newClass->createClass();
+						if($newClass){ 
+							echo '<div class="success">Success!</div>';
+							echo "<script>window.open('add_class.php','_self') </script>"; // reloads page
+						}
+					}
+				?>
+			</div>
+		</form>
+	</main>
 </div>
-
-<div>
-	<label for="ClassNO">Class Number</label>
-	<input type="text" name="ClassNO" id="ClassNO" placeholder="ITEC Number" required>
-</div>
-
-<div>
-	<label for="ClassName">Class Name</label>
-	<input type="text" name="ClassName" id="ClassName" placeholder="Class Name" required>
-</div>
-
-<div>
-	<label for="ExpDate">Class Expires</label>
-	<input type="date" name="ExpDate" id="ExpDate" placeholder="Expiration Date" required>
-</div>
-
-<div><?php
-// -- calls dropdown box  --  drop_do.php
-// -- lists all semesters for instructor to choose from.
-// -- could do similar for class names 
-
-$rows=$dropdo->semSelect();
-echo '<select name="SemesterID" required>'; // Open
-foreach ($rows as $ddo) {
-  echo '<option value="'.$ddo['SemesterID'].'">'.$ddo['SemesterName']." ".$ddo['Year'].'</option>';
-}
-  echo '</select>';// Close
-
-?></div>
-
-
-</div> <div class="one"> <input type="submit" value="Add Class" name="AddClass" id="AddClass"></div>
-
-<div>
-</br>
-Visit:
-<br/>
-<a href="classes.php">MY Classes</a><br/>
-<a href="all_classes.php">All Classes</a><br/>
-<a href="facultyDashboard.php">Home</a><br/>
-</div>
-
-</div>
-
-<?php 
-if(isset($_POST['AddClass'])){
-$newClass = new Classes(array(
-	"LoginID" => $_SESSION["LoginID"],
-	"ClassID" => $_POST["ClassID"],
-	"ClassNO" => $_POST["ClassNO"],
-	"ClassName" => $_POST["ClassName"],
-	"SemesterID" => $_POST["SemesterID"],
-	"ExpDate" => $_POST["ExpDate"]
-));
-$newClass->createClass();
-}
-?>
-
-</form>
-</main>
-</div>
-<?php include("../_templates/footer.php");?>
+<?php include('../_templates/facfooter.php');?>
 </body>
 </html>
