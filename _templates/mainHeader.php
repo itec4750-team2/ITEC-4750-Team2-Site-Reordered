df6028f4f33f742f8a93fd43c32a3e8feb04b2be
Sarah Goodman <sarah.goodman@mga.edu>
2017-09-12 21:15:52 -0400

    Redid pages with bootstrap, removed css code, added altered image

--- _templates/mainHeader.php
+++ _templates/mainHeader.php
@@ -8,19 +8,24 @@ include('_php/session.php');
 <head>
 	<meta charset="utf-8">
 	<title>Knightly Knowledge</title>
+	<link rel="stylesheet" href="_css/bootstrap.min.css" />
 	<link href="_css/style.css" rel="stylesheet" />
 </head>
 <body>
-	
+
     <!--Header (school logo) -->
 	<div>
 	<header>
-		<img src="_images/MiddleGeorgia_Inst_EXHoriz_DkBgrnd.jpg" alt="MGA Banner" />
+		<img src="_images/MiddleGeorgia_Inst_EXHoriz_DkBgrnd.jpg" alt="MGA Banner" class="img-fluid" />
 	</header>
 	<div>
-	<!--Header (School logo - left-hand side) -->	
-	<div id="loginLogo">
-		<img src="_images/knightly knowledge logo.jpg" alt="MGA Knightly Knowledge Logo" width="25%" />
-	</div>
-	
 
+	<!-- Main Content Section-->
+	<main>
+		<!-- container class for pages -->
+		<div class="container-fluid float-left" style="padding: 20px 0px 15px 0px;">
+
+		<!--Header (School logo - left-hand side) -->
+			<div id="loginLogo" class="col-sm-6">
+				<img src="_images/knightly knowledge logo.jpg" alt="MGA Knightly Knowledge Logo"/>
+			</div>
