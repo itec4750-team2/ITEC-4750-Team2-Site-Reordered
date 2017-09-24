142556ecb16dd8fcc56d9a044a53fb4cea83cda7
Kateknm <31115258+Kateknm@users.noreply.github.com>
2017-09-24 02:50:57 -0700

    Hide empty tables

--- _facultyPages/stud_mgmt_pg.php
+++ _facultyPages/stud_mgmt_pg.php
@@ -51,50 +51,56 @@ require($_SERVER['DOCUMENT_ROOT'].'/_php/_models/class_assign_model.php');
 							<!-------------------------------------------------------------------->
 				<!-- ------------- Student Classes Info ----------->
 			<h2>Assigned Classes & Groups</h2>
-
-			<table>
-				<tr>
-					<th>Class ID</th>
-					<th>Class Number</th>
-					<th>Class Name</th>
-					<th>Instructor</th>
-					<th>Semester</th>
-					<th>Class Expire Date</th>
-					<th></th>
-				</tr>
-
-				<?php
+			<?php	
 				//calls class data object and loads table data by LoginID
 				$classdo = new Class_DO();
 				$rows=$classdo->loadByLoginID($StID);
-				//builds table with class data	
-				foreach ($rows as $value){
-					echo '<tr>';
-						echo '<td><a href="class_page.php?cid='.$value['ClassID'].'">'.$value['ClassID'].'</a></td>'; // links back to class_page.php
-						echo '<td>'.$value['ClassNO'].'</td>';
-						echo '<td>'.$value['ClassName'].'</td>';
-						echo '<td>';
-							$instr = new CA_DO();
-							$facs=$instr->listClassInstrs($value['ClassID']);
-							$i = 0;
-							foreach ($facs as $val){
-								// ++++ Change: Added | for multiple instructors 9/9 KM ++++
-								if($i == 0){
-									echo $val['FName']. ' ' .$val['LName'];
-								}
-								else{
-									echo ' | '. $val['FName']. ' ' . $val['LName'];
-								}
-								$i++;	
-							}
-						echo'</td>';
-						echo '<td>'.$value['SemesterName'].' '.$value['Year'].'</td>';
-						echo '<td>'.$value['ExpDate'].'</td>';
-						echo '<td><a href="../_php/del_class_assignment.php?cid='.$value['ClassID'].'&stid='.$StID.'"><img class ="small_icon" src="../_images/delete.png" alt="Delete"></a></td>'; // delete class assignment
-					echo '</tr>';
+				// ++++ Change: Added if statement to hide table if empty 9/24 KM ++++
+				if(!empty($rows)){
+					?>		
+					<table>
+						<tr>
+							<th>Class ID</th>
+							<th>Class Number</th>
+							<th>Class Name</th>
+							<th>Instructor</th>
+							<th>Semester</th>
+							<th>Class Expire Date</th>
+							<th></th>
+						</tr>
+
+						<?php
+
+						//builds table with class data	
+						foreach ($rows as $value){
+							echo '<tr>';
+								echo '<td><a href="class_page.php?cid='.$value['ClassID'].'">'.$value['ClassID'].'</a></td>'; // links back to class_page.php
+								echo '<td>'.$value['ClassNO'].'</td>';
+								echo '<td>'.$value['ClassName'].'</td>';
+								echo '<td>';
+									$instr = new CA_DO();
+									$facs=$instr->listClassInstrs($value['ClassID']);
+									$i = 0;
+									foreach ($facs as $val){
+										// ++++ Change: Added | for multiple instructors 9/9 KM ++++
+										if($i == 0){
+											echo $val['FName']. ' ' .$val['LName'];
+										}
+										else{
+											echo ' | '. $val['FName']. ' ' . $val['LName'];
+										}
+										$i++;	
+									}
+								echo'</td>';
+								echo '<td>'.$value['SemesterName'].' '.$value['Year'].'</td>';
+								echo '<td>'.$value['ExpDate'].'</td>';
+								echo '<td><a href="../_php/del_class_assignment.php?cid='.$value['ClassID'].'&stid='.$StID.'"><img class ="small_icon" src="../_images/delete.png" alt="Delete"></a></td>'; // delete class assignment
+							echo '</tr>';	
+							echo '</table>';
+						}
 				}
 				?>					
-			</table>
+		
 			<br/>
 				<!-- ------------- Student Group Info ----------->
 			<table>
