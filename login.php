ced78c2cf3d5b73ef2d7c0ccaccaf3b12dff4aab
Kateknm <31115258+Kateknm@users.noreply.github.com>
2017-09-29 19:21:30 -0700

    Login & Password
    see test site @ https://markham-surveyengine.herokuapp.com/login.php

--- login.php
+++ login.php
@@ -3,7 +3,7 @@ include($_SERVER['DOCUMENT_ROOT'].'/_php/config.php');
 include($_SERVER['DOCUMENT_ROOT'].'/_templates/mainHeader.php');
 include($_SERVER['DOCUMENT_ROOT'].'/_php/_objects/login_do.php');
 ?>
-	<!-- Main tag starts in mainHeader.php -->
+<main>
 		<div class="row">
 			<div class="col-md-5">
 				<form action="#" method="post" id="loginForm" class="form-horizontal">
@@ -32,36 +32,36 @@ include($_SERVER['DOCUMENT_ROOT'].'/_php/_objects/login_do.php');
 					<!-- Submit form  -->
 					<div class="form-group">
 						<div class="col-sm-offset-2 col-sm-9">
-							<input type="submit" value="Login" name="Login" class="btn btn-primary btn-lg submitMain">
+							<input type="submit" value="Login" name="Login" class="btn btn-primary btn-lg submit">
 						</div>
 					</div>
 					<!-- ++++ Change: Removed errorBlock handled in Login_DO rather than SESSION ++++ -->
 					</fieldset>
 				</form>
-
+				
 					<!-- ++++ Change: Refactoring login functions ++++ -->
-					<?php
+					<?php 
 					// --- Calls Login_DO to handle login
 					if(isset($_POST['Login']) && (!empty($_POST['Email'])) && (!empty($_POST['Pword']))){
 						$email = $con->real_escape_string($_POST['Email']);
 						$password = $con->real_escape_string( $_POST['Pword']);
-
-						$userdo = new Login_DO();
-						$user=$userdo->getUser($email);  // check for user
-						$Subj = $user['LoginID'];
+						
+						$userdo = new Login_DO();		
+						$user=$userdo->getUser($email);  // check for user		
+						$Subj = $user['LoginID']; 
 
 						if(!empty($Subj)){
 							$signin = new Login_DO();
 							$accts=$signin->signIn($Subj, $password);
-						}
-					}
+						}				
+					}	
 					// --- Msg for Empty Email or Password --->
 					if(isset($POST['Login']) && ((empty($_POST['Email'])) || (empty($_POST['Pword'])))){
 						echo '<div class="error">Please Enter Email & Password.</div>';
 					}
-					?>
+					?>				
 			</div>
-		</div>
-	</div> <!-- End container div that started in mainHeader.php -->
-</main>	<!-- End main that started in mainHeader.php-->
+		</div> <!-- End container div that started in mainHeader.php -->
+	</div>
+</main>	<!-- End main -->
 <?php include('_templates/footer.php');?>
