26a1cc5e202bcce8b972217637636cc92cee7e62
Kateknm <31115258+Kateknm@users.noreply.github.com>
2017-09-29 19:20:49 -0700

    Update Student Profile
    see test site @ https://markham-surveyengine.herokuapp.com/login.php

--- _studentPages/classes_student.php
+++ _studentPages/classes_student.php
@@ -1,50 +1,50 @@
-<?php
+<?php 
 include('../_templates/studentHeader.php');
 include('../_templates/studentNav.php');
-require("../_php/_objects/class_do.php");
+require("../_php/_objects/class_do.php");	
 	//$root = realpath($_SERVER["DOCUMENT_ROOT"]);
-	//require("$root/_php/_objects/class_do.php");
+	//require("$root/_php/_objects/class_do.php");  
 ?>
+	</div>
+	<!-- Main Content Section-->
+	<div class="wrapper">
 
-<h2 class="center">Your Classes</h2>
 <!-- Builds table for classes. If classes have Expired the are not pulled. KM 9/2/17 -->
-<div class="container-fluid" style="padding: 20px 0px 15px 0px;">
-<?php
+<div id="main">
+	<h2 class="center">Your Classes</h2>
+	  
+	<?php
 	if(isset($_SESSION['LoginID'])){
 	$classdo = new Class_DO();
 	}
-?>
-	<div class="row">
-		<div class="col-md-7 col-centered">
-			<table class="table table-striped">
-				<thead>
-					<tr>
-						<th>Class ID</th>
-						<th>Class Number</th>
-						<th>Class Name</th>
-						<th>Semester</th>
-						<th>Class Expire Date</th>
-					</tr>
-				</thead>
-				<tbody>
-					<?php
-						//calls class data object and loads table data by LoginID
-						$rows=$classdo->loadByLoginID($_SESSION['LoginID']);
+	?>
 
-						//builds table with class data
-						foreach ($rows as $value){
-							echo "<tr><td>".$value['ClassID']."</td>";
-							echo "<td>".$value['ClassNO']."</td><td>".$value['ClassName']."</td>";
-							echo "<td>".$value['SemesterName']. " ".$value['Year']."</td>";
-							echo "<td>". $value['ExpDate']."</td></tr>";
-						}
-					?>
-				</tbody>
-			</table>
-		</div>
+	<table>
+		<tr>
+			<th>Class ID</th>
+			<th>Class Number</th>
+			<th>Class Name</th>
+			<th>Semester</th>
+			<th>Class Expire Date</th>
+		</tr>
+
+	<?php
+	//calls class data object and loads table data by LoginID
+	$rows=$classdo->loadByLoginID($_SESSION['LoginID']);
+
+	//builds table with class data	
+	foreach ($rows as $value){
+		echo "<tr><td>".$value['ClassID']."</td>";
+		echo "<td>".$value['ClassNO']."</td><td>".$value['ClassName']."</td>";
+		echo "<td>".$value['SemesterName']. " ".$value['Year']."</td>";
+		echo "<td>". $value['ExpDate']."</td></tr>";
+	}
+	?>		
+
+	</table>
 	</div>
 </div>
-
+	
 <?php include("../_templates/footer.php");?>
 </body>
 </html>
