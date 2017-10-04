77bf0385b595b9d925b793e672e707488403c940
Michael Matthews <mm042676@gmail.com>
2017-10-04 14:23:58 -0400

    fixes for issue 48

--- _php/config.php
+++ _php/config.php
@@ -1,32 +1,39 @@
-      <?php
-	 // $con and $server to be used throughout site
-	 // ++++ Change: Added $server variable to make it easy to change reference paths.
+<html>
+<body>
+<?php
+	// $con and $server to be used throughout site
+	// ++++ Change: Added $server variable to make it easy to change reference paths.
 	$server ="//{$_SERVER['SERVER_NAME']}";	
-		 //-----------LOCAL CONNECTION-------------
-		/*
-		 //connection info
-         	 $dbhost = 'localhost:3306';
-		 $dbname = 'mga_db'; 
-         	 $dbuser = 'user1';
-          	 $dbpass = 'thisuser';
-		 $con = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
-		 */
-		 //-------------------------------------------
 
-	 //-------------SERVER CONNECTION-------------
-	 //connection info
-	 $dbhost = '45.55.170.45';
-	 $dbname = 'mga_db'; 
-	 $dbuser = 'katie';
-	 $dbpass = 'katiepass';
+	// Lets try Katies DB first
+	// This should be simplified when this is placed into production to simply query 1 database
+	
+	//-------------SERVER CONNECTION-------------
+	//connection info
+	$dbhost = '45.55.170.45';
+	$dbname = 'mga_db'; 
+	$dbuser = 'katie';
+	$dbpass = 'katiepass';
 
-	 //connect to db
-	 $con = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname); 
-	 //-------------------------------------------
+	//connect to db
+	@$con = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname); 
+	//-------------------------------------------
 
-         if(! $con ){
-            die('Could not connect: ' . mysqli_error());
-         }
-      ?>
-   </body>
+	 if(mysqli_connect_errno()){ // failed to connect to katies database; lets try local host next
+		//-------------SERVER CONNECTION-------------
+		//connection info
+		$dbhost = 'localhost:3306';
+		$dbname = 'mga_db'; 
+		$dbuser = 'user1';
+		$dbpass = 'thisuser';
+		$con = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname); 
+		if(mysqli_connect_errno()){  // failed to connect to local db too
+			die('Could not connect: ' . mysqli_connect_error());
+		}
+		else {
+			echo "<script type = 'text/javascript'>alert('Online DB unavailable, connected to local DB');</script>";
+		}
+	 }
+	?>
+</body>
 </html>
