1af210c47c07cc16c7b1363943c05ebc7a7b5e7f
Kateknm <31115258+Kateknm@users.noreply.github.com>
2017-10-05 07:46:14 -0700

    Added  Update Delete Un-Assign to pages. (#43)
    * all_classes class_page classes
    
    Added Update Delete Un-Assign to pages.
    
    * Update config.php

--- _php/config.php
+++ _php/config.php
@@ -3,23 +3,23 @@
 	 // ++++ Change: Added $server variable to make it easy to change reference paths.
 	$server ="//{$_SERVER['SERVER_NAME']}";	
 		 //-----------LOCAL CONNECTION-------------
-		/*
+	/*
 		 //connection info
          	 $dbhost = 'localhost:3306';
 		 $dbname = 'mga_db'; 
          	 $dbuser = 'user1';
           	 $dbpass = 'thisuser';
-		 $con = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
-		 */
-		 //-------------------------------------------
 
+		 //-------------------------------------------
+	 */
 	 //-------------SERVER CONNECTION-------------
-	 //connection info
+		 //connection info
 	 $dbhost = '45.55.170.45';
 	 $dbname = 'mga_db'; 
 	 $dbuser = 'katie';
 	 $dbpass = 'katiepass';
-
+	
+		
 	 //connect to db
 	 $con = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname); 
 	 //-------------------------------------------
