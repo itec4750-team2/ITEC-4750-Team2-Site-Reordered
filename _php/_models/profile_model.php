a6297f80d71033b6b45b1f67bd79e627dc0cf92f
Kateknm <31115258+Kateknm@users.noreply.github.com>
2017-10-07 13:52:49 -0700

    add_student page
    Allows inst to add new student from class page and adds student to class.

--- _php/_models/profile_model.php
+++ _php/_models/profile_model.php
@@ -3,7 +3,9 @@
 include($_SERVER['DOCUMENT_ROOT'].'/_php/config.php');
 
 class Profile{
-private $LoginID;	
+private $LoginID;
+private $Role;
+private $Password;	
 private $Subj;  
 private $Email;
 private $FName;
@@ -18,16 +20,20 @@ public function __get($property){
 public function __construct($arr){
 if(isset($arr['LoginID'])){
 	$this->LoginID = $arr['LoginID'];
+	$this->Role = $arr['Role'];
+	$this->Password = $arr['Password'];
 	$this->Subj = $arr['Subj'];	  
 	$this->Email = $arr['Email'];
 	$this->FName = $arr['FName'];
-	$this->LName= $arr['LName'];
+	$this->LName = $arr['LName'];
 	$this->do = new Profile_DO();
 }}
 
 private function all_params(){
 	$params = array(
 	'LoginID' => $this->LoginID,
+	'Role' => $this->Role,
+	'Password' => $this->Password,
 	'Subj' => $this->Subj,
 	'Email' => $this->Email,
 	'FName' => $this->FName,
@@ -45,5 +51,8 @@ public function deleteStudent(){
 	return $this->do->deleteStudent($this->all_params());
 }
 
+public function addProfile(){
+	return $this->do->addProfile($this->all_params());
+}
 }
  ?>
--- _php/del_class_assignment.php
+++ _php/del_class_assignment.php
@@ -1,29 +1,21 @@
 <?php	
-// ++++ Change: Added del_class_assignment.php 9/7 KM ++++
+// ++++ Change: Added del_group_assignment.php 9/7 KM ++++
 // Supports stud_mgmt_pg 
 include('../_templates/facultyHeader.php');
 include('../_templates/facultyNav.php');
 require("../_php/_objects/class_assign_do.php");
 require("../_php/_models/class_assign_model.php");
-// ++++ Change: If statements to distinguish between faculty and student un-assign.php 9/29 KM ++++
-if(!empty($_GET['stid'])){
-$Subj= $_GET['stid']; // Student
-}
-if(!empty($_GET['fid'])){
-$Subj= $_GET['fid']; // facultyHeader
-}
+
+$StID = $_GET['stid']; // Student
 $LoginID = $_SESSION['LoginID']; // Current User
 $ClassID = $_GET['cid'];
 
 $DeleteClassA = new Class_Assign(array( 
-	'Subj' => $Subj, 
+	'Subj' => $StID, 
 	'LoginID' => $LoginID, 
 	'ClassID' => $ClassID));	
 $DeleteClassA->delClassA();
 
-// ++++ Change: If statements to distinguish between faculty and student to reload appropriate page.php 9/29 KM ++++
-if(!empty($_GET['stid'])){echo "<script>window.open('../_facultyPages/stud_mgmt_pg.php?stid=$StID', '_self') </script>";}	
-if(!empty($_GET['fid'])){echo "<script>window.open('../_facultyPages/classes.php','_self') </script>";}
-
+echo "<script>window.open('../_facultyPages/stud_mgmt_pg.php?stid=$StID', '_self') </script>";	
 
 ?>
