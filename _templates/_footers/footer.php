521482041a5d28269e79a0cf239bd0cb11686797
Kateknm <31115258+Kateknm@users.noreply.github.com>
2017-11-16 20:04:49 -0800

    MISC
    Miscellaneous Changes

---
+++ _templates/_footers/footer.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+
+<head>
+		<link href="../_css/footerstyle.css" rel="stylesheet" />
+</head>
+<footer>
+		<div id="purpleBar">
+			<div class="row">
+				<div class="col-sm-6 text-center">478.471.2700 | <a href="https://www.google.com/maps/dir/100 university parkway macon ga 31206" style="color: #FFFFFF">100 University Parkway | Macon, GA 31206</a></div>
+				<div class="col-sm-6 text-center">
+					<a href="https://www.facebook.com/MiddleGeorgiaStateUniversity2"> <img src="<?php echo $server.'/_images/facebook.jpg';?>" height="30" width="30" alt="f"></a>
+					<a href="https://twitter.com/MGAstateU"> <img src="<?php echo $server.'/_images/twitter.jpg';?>" height="30" width="30" alt="t"</a>
+					<a href="https://www.instagram.com/mgastateu/"> <img src="<?php echo $server.'/_images/instagram.jpg';?>" height="30" width="30" alt="i"</a>
+					<a href="https://www.youtube.com/user/mgaknights"> <img src="<?php echo $server.'/_images/youtube.jpg';?>" height="30" width="30" alt="y"</a>
+				</div>
+			</div>
+		</div>
+			
+</footer>
+</body>
+</html>
