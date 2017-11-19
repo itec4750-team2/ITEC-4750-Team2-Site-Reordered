12e4bf600fba643976484461d4d94c0bbcf58a4e
Kateknm <31115258+Kateknm@users.noreply.github.com>
2017-11-18 23:51:57 -0800

    Style Student Classes Page

--- _studentPages/classes_student.php
+++ _studentPages/classes_student.php
@@ -5,12 +5,8 @@ include('../_templates/studentHeader.php');
 include('../_templates/studentNav.php');
 require("../_php/_objects/class_do.php");	
 ?>
-	</div>
-	<!-- Main Content Section-->
-	<div class="wrapper">
 
 <!-- Builds table for classes. If classes have Expired the are not pulled. KM 9/2/17 -->
-<div id="main">
 	<h2 class="center">Your Classes</h2>
   
 	<?php
@@ -18,32 +14,41 @@ require("../_php/_objects/class_do.php");
 		$classdo = new Class_DO();
 	}
 	?>
+		<div class="row">
+			<div class="col-md-10 col-centered table-responsive">		
+			<table class="table-responsive">
+			<thead>
+				<tr>
+					<th class="col-sm-1">ID</th>
+					<th class="col-sm-2">Number</th>
+					<th class="col-sm-5">Name</th>
+					<th class="col-sm-1">Semester</th>
+					<th class="col-sm-2">Expire Date</th>
+				</tr>
+				</thead>
+			<tbody>
+			<?php
+			//calls class data object and loads table data by LoginID
+			$rows=$classdo->loadByLoginID($_SESSION['LoginID']);
 
-	<table>
-		<tr>
-			<th>Class ID</th>
-			<th>Class Number</th>
-			<th>Class Name</th>
-			<th>Semester</th>
-			<th>Class Expire Date</th>
-		</tr>
-
-	<?php
-	//calls class data object and loads table data by LoginID
-	$rows=$classdo->loadByLoginID($_SESSION['LoginID']);
-
-	//builds table with class data	
-	foreach ($rows as $value){
-		echo "<tr><td>".$value['ClassID']."</td>";
-		echo "<td>".$value['ClassNO']."</td><td>".$value['ClassName']."</td>";
-		echo "<td>".$value['SemesterName']. " ".$value['Year']."</td>";
-		echo "<td>". $value['ExpDate']."</td></tr>";
-	}
-	?>		
-
+			//builds table with class data	
+			foreach ($rows as $value){
+				echo '<tr>';
+				echo '<td class="col-sm-1">'.$value['ClassID'].'</td>';
+				echo '<td class="col-sm-2">'.$value['ClassNO'].'</td>';
+				echo '<td class="col-sm-5">'.$value['ClassName'].'</td>';
+				echo '<td class="col-sm-1">'.$value['SemesterName']. ' '.$value['Year'].'</td>';
+				echo '<td class="col-sm-2">'. $value['ExpDate'].'</td>';
+				echo '</tr>';
+			}
+			?>		
+	</tbody>
 	</table>
 	</div>
-</div>
+	</div>
+	<br/>
+	<br/>
+
 	
 <?php include("../_templates/_footers/footer.php");?>
 </body>
