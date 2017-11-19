57ba221c8b4cae64552fdc610fcd68b1e83a1329
Kateknm <31115258+Kateknm@users.noreply.github.com>
2017-11-18 18:46:54 -0800

    update_class styling
    update_class styling updated

--- _facultyPages/update_class.php
+++ _facultyPages/update_class.php
@@ -18,8 +18,7 @@ $P = 'update_class';
 <!-- Main Content Section-->
 <main>
 	<div class="container-fluid" style="padding: 20px 0px 15px 0px;">
-		<div class="row">
-			<div class="col-md-5 col-centered">
+
 				<?php
 				// ++++ Change: Added Catch for not logged in and ClassID missing 9/30 KM ++++
 				if(isset($_GET['cid'])){$ClassID = $_GET['cid'];}
@@ -31,17 +30,18 @@ $P = 'update_class';
 						else{
 				?>
 							<!-- ++++ Change: Created reusable modules & included in form 9/30 KM ++++-->
-							<form name="update-class" method="POST" class="form-horizontal">
+							<fieldset><form name="update-class" method="POST" class="form-horizontal">
 								<!--------------------------- Get Class Info ------------------------>
 								<?php include($_SERVER['DOCUMENT_ROOT'].'/_templates/_read/class_information.php');?>
 								<br/>
-								<h3><b>Update Class Information</b></h3>
+								<legend id="table-caption">Update Class Information</legend>
 								<!--------------- Add Instructor Assignment ------------------>
 								<?php include($_SERVER['DOCUMENT_ROOT'].'/_templates/_create/instructor_assign.php');?>
 								<!--------------- Remove Instructor Assignment ------------------>
 								<?php include($_SERVER['DOCUMENT_ROOT'].'/_templates/_delete/instructor_remove.php');?>
 								<!---------------------------- updateClass Form --------------------->
 								<?php include($_SERVER['DOCUMENT_ROOT'].'/_templates/_update/updateClass.php');	?>
+								</fieldset>
 							</form>
 				<?php }} // End else !empty LoginID & ClassID?>
 			</div>
